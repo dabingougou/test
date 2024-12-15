e4df02ea32175567cd6137dcc101165eb9c6b2ae
Huibin <huibin.chang@berkeley.edu>
2024-12-15 13:33:23 -0800

    update

--- python/prime_factors.py
+++ python/prime_factors.py
@@ -3,10 +3,14 @@
 n = 1234567890
 
 def find_factors(n):
+  m = 1
   p = 1
   if n <= 0:
     return 0
-  elif:
+  else:
+        while m < n:
+            m += 1 
+            
     while n%(p + 1) == 0:
       p += 1
 # To be completed
