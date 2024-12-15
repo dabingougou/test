fad6c6f6bc2e0b1b1e282bd9078cf8103d43f8b0
Huibin <huibin.chang@berkeley.edu>
2024-12-15 13:28:35 -0800

    First file added

---
+++ python/prime_factors.py
@@ -0,0 +1,15 @@
+# Python code to find unique prime factors
+
+n = 1234567890
+
+def find_factors(n):
+  p = 1
+  if n <= 0:
+    return 0
+  elif:
+    while n%(p + 1) == 0:
+      p += 1
+# To be completed
+
+   
+
